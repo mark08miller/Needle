b2dc393a017b9ffc9e21ae6edeb1b7fb3673fc4f
Mark MIller <youtte1g@gmail.com>
2022-11-07 20:54:49 +0200

    final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] name = {"Link", "needle"};
+        String name1 = findNeedle(name);
+        System.out.println(name1);
+    }
+    public static String findNeedle(Object[] haystack) {
+        int i = 0;
+        while(!String.valueOf(haystack[i]).equals("needle")){
+            i++;
+        }
+        return "found the needle at position " + i;
     }
 }
